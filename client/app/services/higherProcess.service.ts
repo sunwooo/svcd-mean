3db86fa7f1e223179220ad6ab6a810d05fbf0cc8
sunwooo <psw@isu.co.kr>
2018-08-10 11:30:42 +0900

    #PSW SERVICE 추가

---
+++ client/app/services/higherProcess.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { NgForm } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable()
+export class HigherProcessService {
+
+    constructor(private http: HttpClient,
+        private cookieService: CookieService
+    ) { }
+
+    /**
+     * 상위업무리스트 가져오기 
+    */
+    
+    getHigherProcessList(condition): Observable<any> {
+        console.log("===========================getHigherProcessList", condition);
+        var httpParams = new HttpParams({ fromObject: condition });
+        return this.http.get<any>('/api/higherProcess/list', {params: httpParams});
+    }
+
+    /**
+     * 상위업무 수정
+     * @param form 
+    */
+    putHigherProcess(form: NgForm): Observable<any> {
+        return this.http.put<any>('/api/higherProcess/update', form.value, {withCredentials:true});
+    }
+
+    /**
+     * 삭제하기
+     * @param higherProcessId
+     */
+    delete(higherProcessId): Observable<any>{
+        var body = {id:higherProcessId};
+        var httpOptions = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' }), body: body
+        };
+        return this.http.delete<any>('/api/higherProcess/delete', httpOptions);
+    }
+}
