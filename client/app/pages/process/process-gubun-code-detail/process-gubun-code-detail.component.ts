079fab26e5eec920a8a9d0ee293aecef073fe0fa
sykim <ksy0226@isu.co.kr>
2018-09-18 15:23:41 +0900

    #KSY, 처리구분코드 상세보기

--- client/app/pages/process/process-gubun-code-detail/process-gubun-code-detail.component.ts
+++ client/app/pages/process/process-gubun-code-detail/process-gubun-code-detail.component.ts
@@ -4,6 +4,7 @@ import { ToastComponent } from '../../../shared/toast/toast.component';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CommonApiService } from '../../../services/common-api.service';
 
 @Component({
     selector: 'app-process-gubun-code-detail',
@@ -17,6 +18,11 @@ export class ProcessGubunCodeDetailComponent implements OnInit {
     @Input() dValues;  //모달창 무시용
     @Output() openerReload = new EventEmitter<any>(); //삭제 후 다시 조회를 위한 이벤트
 
+    public isLoading = true;
+    public higherObj: any = [];                     //상위업무리스트
+    public higher_cd: string = "*";                 //상위업무코드
+    public higher_nm: string = "";
+    public selectedIdx = 0;                      //Object 내 index
     private formData: any = {};    //전송용 formData
 
     public question_typeObj: { name: string; value: string; }[] = [
@@ -29,14 +35,50 @@ export class ProcessGubunCodeDetailComponent implements OnInit {
     ];
 
     public use_ynObj: { name: string; value: string; }[] = [
-        { name: '사용', value: 'Y' },
-        { name: '미사용', value: 'N' }
+        { name: '사용', value: '사용' },
+        { name: '미사용', value: '미사용' }
     ];
 
     constructor(private processGubunCodeService: ProcessGubunCodeService
-        , public toast: ToastComponent) { }
+        , public toast: ToastComponent
+        , private commonApi: CommonApiService) { }
 
     ngOnInit() {
+        this.isLoading = false;
+        this.getHigherProcess();
+    }
+
+    /**
+   * 상위업무 변경 시 
+   */
+    setHigherCd(idx) {
+        this.processGubunCodeDetail.higher_cd = this.higherObj[idx].higher_cd;
+        this.processGubunCodeDetail.higher_nm = this.higherObj[idx].higher_nm;
+    }
+
+    /**
+   * 상위업무리스트 조회
+   */
+
+    getHigherProcess() {
+        this.commonApi.getHigher({ scope: "*" }).subscribe(
+            (res) => {
+                this.higherObj = res;
+
+                //idx를 찾아서 조회시 초기값 세팅
+                var tmpIdx = 0;
+                this.higherObj.forEach(element => {
+                    if (element.higher_cd == this.processGubunCodeDetail.higher_cd) {
+                        //console.log("element.company_cd : ", element.company_cd);
+                        //console.log("this.processGubunCodeDetail.higher_cd : ", this.processGubunCodeDetail.higher_cd);
+                        this.selectedIdx = tmpIdx;
+                    }
+                    tmpIdx++;
+                });
+            },
+            (error: HttpErrorResponse) => {
+            }
+        );
     }
 
     /**
