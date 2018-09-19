68b6d3a5cdda04822e0e5ee2a6285a078be126d9
sykim <ksy0226@isu.co.kr>
2018-09-19 14:09:11 +0900

    #KSY, 주석제거

--- dist/server/app/controllers/api/user.js
+++ dist/server/app/controllers/api/user.js
@@ -335,7 +335,7 @@ module.exports = {
         });
       }], function (err, totalCnt) {
 
-        console.log("user controller search.findUsermanage : ", search.findUsermanage);
+        //console.log("user controller search.findUsermanage : ", search.findUsermanage);
         User.find(search.findUsermanage, function (err, user) {
             if (err) {
 
