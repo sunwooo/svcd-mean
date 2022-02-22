90d972d24d85018ce5d13b6b1ef7b852cf7628fe
sunwooo <psw@isu.co.kr>
2022-02-22 16:44:23 +0900

    #psw 22-02-22 건설erp 문의시, 디폴트 양식 적용

--- client/app/pages/incident/incident-new/incident-new.component.ts
+++ client/app/pages/incident/incident-new/incident-new.component.ts
@@ -36,6 +36,10 @@ export class IncidentNewComponent implements OnInit {
     private attach_file: any = []; //mongodb 저장용 첨부파일 배열
     private dateChange = false; //완료요청일이 변경되었는지 여부
 
+    //2022-02-22 psw 추가
+    //public new_content: string = "아래의 형식에 맞추어 작성해주시기 바랍니다.";
+    public new_content: string = "<table style='border: 1px rgb(0, 0, 0); border-image: none; width: 813px; height: 428.1px; border-collapse: collapse; -ms-word-break: break-all;' border='1' cellspacing='0' cellpadding='0'><tbody><tr><td style='border-width: 0px 0px 1px; border-style: solid; border-color: rgb(0, 0, 0); border-image: none; width: 811px; height: 30px; background-repeat: no-repeat; background-color: rgb(255, 255, 255);' colspan='2'><p><span style='font-size: 13pt;'><strong>&nbsp;<span style='color: rgb(255, 0, 0);'><u>※ 아래의 형식에 맞추어 작성해 주시기 바랍니다. </u></strong></span></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 79.55px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>화면 경로<br></strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 79.55px;'><p><span style='font-size: 12pt;'>&nbsp;관련 화면 경로 작성 </span></p><p><span style='font-size: 12pt;'>&nbsp;(예시) 회계&gt;전표통제&gt;귀속부서관리&gt;사용자 추가</span></p><p><span style='font-size: 12pt;'>​</span><br></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 75px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>요청&nbsp;사항</strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 75px;'><p><span style='font-size: 12pt;'>&nbsp;요청사항을 상세히 작성 요망<br></span></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 93px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>문제점<br></strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 93px;'><p><span style='font-size: 12pt;'>&nbsp;현재 발생하고 있는 문제의 상황을 상세히&nbsp;작성 요망</span><br></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 76px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>예상 결과<br></strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 76px;'><p><span style='font-size: 12pt;'>&nbsp;예상하는 결과 작성 <br></span></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 25px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>기타 사항<br></strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 25px;'><p><span style='font-size: 12pt;'><br></span><br></p></td></tr><tr><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 163px; height: 51px; background-color: rgb(255, 245, 153);'><p style='text-align: center;'><strong style='font-size: 12pt;'>요청자&nbsp;정보<br></strong></p></td><td style='border: 1px solid rgb(0, 0, 0); border-image: none; width: 648px; height: 51px;'><p><span style='font-size: 12pt;'>&nbsp;(예시) 휴대폰 : 010-123-4567 / 이메일 : <a style='color: rgb(0, 0, 0);' href='mailto:test@isu.co.kr'>test@isu.co.kr</a> &nbsp;<br></span></p></td></tr></tbody></table><p><span style='font-size: 9pt;'>​</span></p>";
+    //추가 끝
     public uploader: FileUploader = new FileUploader({ url: URL }); //file upload용 객체
     public processSpeed: { name: string; value: string; }[] = [
         { name: '보통', value: 'N' },
@@ -61,10 +65,12 @@ export class IncidentNewComponent implements OnInit {
       
         //$('#summernote').summernote();
         $('#summernote').summernote({
-            height: 350, // set editor height;
+            height: 350,     // set editor height;
             minHeight: null, // set minimum height of editor
             maxHeight: null, // set maximum height of editor
-            focus: false // set focus to editable area after initializing summernote
+            focus: false,    // set focus to editable area after initializing summernote
+            lang : 'ko-KR'   //default: 'en-US'
+            /*
             , toolbar: [
                 ['edit', ['undo', 'redo']],
                 ['headline', ['style']],
@@ -77,6 +83,21 @@ export class IncidentNewComponent implements OnInit {
                 ['table', ['table']],
                 ['view', ['fullscreen', 'codeview']]
             ]
+*/
+            /*
+            , toolbar: [
+                ['edit', ['실행 취소', '다시 실행']],
+                ['headline', ['스타일']],
+                ['style', ['굵게', '기울임꼴', '밑줄', '위 첨자', '아래 첨자', '취소선', '글자 효과 없애기']],
+                ['fontface', ['맑은고딕']],
+                ['textsize', ['글자 크기']],
+                ['fontclr', ['색상']],
+                ['alignment', ['글머리 기호', '번호 매기기', '문단 정렬', '줄간격']],
+                ['height', ['줄간격']],
+                ['table', ['테이블']],
+                ['view', ['전체 화면', '코드 보기']]
+            ]
+            */
             ,popover: {
                 image: [],
                 link: [],
@@ -109,6 +130,7 @@ export class IncidentNewComponent implements OnInit {
             this.addIncident();
 
         }
+        
     }
 
     /**
@@ -133,6 +155,7 @@ export class IncidentNewComponent implements OnInit {
 
         form.value.incident.higher_cd = this.higher.higher_cd;
         form.value.incident.higher_nm = this.higher.higher_nm;
+
         //Template form을 전송용 formData에 저장 
         this.formData = form.value;
 
@@ -187,13 +210,24 @@ export class IncidentNewComponent implements OnInit {
      * @param higher 
      */
     onSelected(higher) {
-        console.log('higherCd : ', higher);
+        //console.log('higherCd : ', higher);
         //this.higherCd = higherCd;
 
         //////////////////////////////////
         this.higher = higher;
         //////////////////////////////////  
 
+        //2022-02-22 psw 추가
+        //상위업무 건설ERP 항목일 경우 
+        //this.toast.open('상위업무 : '+ JSON.stringify(higher.higher_cd), 'success');
+        //x.match(y) === null
+        if(JSON.stringify(higher.higher_cd).match("H006")===null){
+            //this.toast.open('상위업무 : '+ JSON.stringify(higher.higher_cd), 'success');
+            //$('#summernote').summernote('code', "test");
+        }else{
+            $('#summernote').summernote('code', this.new_content);
+        }
+        //추가 끝
     }
 
     /**
